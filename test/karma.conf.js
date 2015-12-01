1eb2bc5c3de4d6775fe117a6bf2ee98f9501e2e4
Wilson Page <wilsonpage@me.com>
2015-12-01 18:06:04 +0000

    Renamed to 'popel' as 'poplar' is tasken on npm

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'poplar.js',
+      'popel.js',
       'test/*.js'
     ],
 
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-/* global suite, sinon, setup, teardown, test, assert, poplar */
+/* global suite, sinon, setup, teardown, test, assert, popel, CharacterData */
 /*jshint maxlen:false*/
 
-suite('poplar >', function() {
+suite('popel >', function() {
   'use strict';
 
   setup(function() {
@@ -27,8 +27,8 @@ suite('poplar >', function() {
   }
 
   test('it can place variables into text nodes', function() {
-    var el = poplar('<div>${foo}</div>');
-    poplar.populate(el, { foo: 'foo' });
+    var el = popel('<div>${foo}</div>');
+    popel.populate(el, { foo: 'foo' });
     assert.equal(el.textContent, 'foo');
   });
 
@@ -36,7 +36,7 @@ suite('poplar >', function() {
     var dataDescriptor, setData, el;
 
     setup(function() {
-      el = poplar('<div>${foo}</div>');
+      el = popel('<div>${foo}</div>');
 
       dataDescriptor = Object.getOwnPropertyDescriptor(
         CharacterData.prototype,
@@ -60,56 +60,56 @@ suite('poplar >', function() {
     });
 
     test('it skips the update if the content is the same', function() {
-      poplar.populate(el, { foo: 'bar' });
-      poplar.populate(el, { foo: 'bar' });
+      popel.populate(el, { foo: 'bar' });
+      popel.populate(el, { foo: 'bar' });
       sinon.assert.calledOnce(setData);
     });
   });
 
   test('it can place variables as partial textContent', function() {
-    var el = poplar('<div>foo: ${foo}</div>');
-    poplar.populate(el, { foo: 'foo' });
+    var el = popel('<div>foo: ${foo}</div>');
+    popel.populate(el, { foo: 'foo' });
     assert.equal(el.textContent, 'foo: foo');
   });
 
   test('it supports dot notation for deep properties', function() {
-    var el = poplar('<div>${one.two.three}</div>');
-    poplar.populate(el, { one: { two: { three: 'boo!' }} });
+    var el = popel('<div>${one.two.three}</div>');
+    popel.populate(el, { one: { two: { three: 'boo!' }} });
     assert.equal(el.textContent, 'boo!');
   });
 
   test('it can place a variable as an attribute', function() {
-    var el = poplar('<div title="${foo}" data-unchanged="unchanged" data-bar="${bar}"></div>');
-    poplar.populate(el, { foo: 'foo', bar: 'bar' });
+    var el = popel('<div title="${foo}" data-unchanged="unchanged" data-bar="${bar}"></div>');
+    popel.populate(el, { foo: 'foo', bar: 'bar' });
     assert.equal(el.getAttribute('title'), 'foo');
     assert.equal(el.dataset.unchanged, 'unchanged');
     assert.equal(el.dataset.bar, 'bar');
   });
 
   test('it can place a variable a attribute partial', function() {
-    var el = poplar('<div title="I am ${foo}"></div>');
-    poplar.populate(el, { foo: 'foo' });
+    var el = popel('<div title="I am ${foo}"></div>');
+    popel.populate(el, { foo: 'foo' });
     assert.equal(el.getAttribute('title'), 'I am foo');
   });
 
   test('it does not call String#replace() for non-partials', function() {
-    var el = poplar('<div title="${foo}"></div>');
+    var el = popel('<div title="${foo}"></div>');
     var replace = this.sinon.spy(String.prototype, 'replace');
-    poplar.populate(el, { foo: 'foo' });
+    popel.populate(el, { foo: 'foo' });
     sinon.assert.notCalled(replace);
     assert.equal(el.getAttribute('title'), 'foo');
   });
 
   test('can re-use parsed element to minimise parseHTML tasks', function() {
-    var parsed = poplar.parse('<h1>${name}</h1>');
+    var parsed = popel.parse('<h1>${name}</h1>');
 
-    var el1 = poplar.create(parsed.cloneNode(true));
-    var el2 = poplar.create(parsed.cloneNode(true));
-    var el3 = poplar.create(parsed.cloneNode(true));
+    var el1 = popel.create(parsed.cloneNode(true));
+    var el2 = popel.create(parsed.cloneNode(true));
+    var el3 = popel.create(parsed.cloneNode(true));
 
-    poplar.populate(el1, { name: '1' });
-    poplar.populate(el2, { name: '2' });
-    poplar.populate(el3, { name: '3' });
+    popel.populate(el1, { name: '1' });
+    popel.populate(el2, { name: '2' });
+    popel.populate(el3, { name: '3' });
 
     assert.equal(el1.textContent, '1');
     assert.equal(el2.textContent, '2');
@@ -124,18 +124,18 @@ suite('poplar >', function() {
       '\n' +
       '  <img src="${bat}" />\n ' +
       '</div>';
-    var el = poplar(template);
+    var el = popel(template);
     assert.equal(totalSize(el), 3);
   });
 
   suite('.populate()', function() {
     test('it returns `false` if element is unknown', function() {
-      assert.isFalse(poplar.populate(document.createElement('div'), {}));
+      assert.isFalse(popel.populate(document.createElement('div'), {}));
     });
 
     test('it results `true` if element is known', function() {
-      var el = poplar('<div>${foo}</div>');
-      assert.isTrue(poplar.populate(el, { foo: 'foo' }));
+      var el = popel('<div>${foo}</div>');
+      assert.isTrue(popel.populate(el, { foo: 'foo' }));
     });
   });
 });
